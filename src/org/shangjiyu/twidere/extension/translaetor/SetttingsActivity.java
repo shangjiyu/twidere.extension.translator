b35bd8e40f085de56a90eecb8348e7a64e16b85e
shangjiyu <shangjiyu@gmail.com>
2013-10-10 22:29:58 +0800

    add sth.

---
+++ src/org/shangjiyu/twidere/extension/translaetor/SetttingsActivity.java
@@ -0,0 +1,60 @@
+/*******************************************************
+ * @Title: SetttingsActivity.java
+ * @Package org.shangjiyu.twidere.extension.translaetor
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author shangjiyu
+ * @date 2013-10-10 下午1:55:28
+ * @version V1.0
+ ********************************************************/
+
+package org.shangjiyu.twidere.extension.translaetor;
+
+import org.shangjiyu.twidere.extension.translator.R;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceActivity;
+
+/********************************************************
+ * @ClassName: SetttingsActivity
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author shangjiyu
+ * @date 2013-10-10 下午1:55:28
+ */
+
+public class SetttingsActivity extends PreferenceActivity implements
+		OnSharedPreferenceChangeListener {
+
+	private SharedPreferences sharedPreferences;
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		addPreferencesFromResource(R.xml.settings);
+		sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+	}
+	
+	
+	/********************************************************
+	 *Title: onSharedPreferenceChanged
+	 *Description: 
+	 * @param sharedPreferences
+	 * @param key
+	 * @see android.content.SharedPreferences.OnSharedPreferenceChangeListener#onSharedPreferenceChanged(android.content.SharedPreferences, java.lang.String)
+	 *********************************************************/
+	
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
+
