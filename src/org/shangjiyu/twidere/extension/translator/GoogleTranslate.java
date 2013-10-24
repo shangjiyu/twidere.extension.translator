7180b90d6398774b9e7e7d5db34933fdf40274d3
shangjiyu <shangjiyu@gmail.com>
2013-10-24 10:29:25 +0800

    clean

--- src/org/shangjiyu/twidere/extension/translator/GoogleTranslate.java
+++ src/org/shangjiyu/twidere/extension/translator/GoogleTranslate.java
@@ -34,7 +34,7 @@ public class GoogleTranslate implements Constants {
 
 	private static final String GOOGLETRANSLATEURL_STRING = Constants.GOOGLETRANSLATEONBAE_STRING;
 	private static final Pattern PATTERN_LINK = Pattern.compile(Constants.NONEED2TRANSLAETPORTION, Pattern.CASE_INSENSITIVE);
-	private static final Pattern PATTERN_ALPHA = Pattern.compile("(11111)");
+	private static final Pattern PATTERN_ALPHA = Pattern.compile("(444)");
 	private final ArrayList<String> linkStrings = new ArrayList<String>();
 	private int uneed2TranslateElementIndex = 0;
 	
@@ -55,15 +55,12 @@ public class GoogleTranslate implements Constants {
 					linkStrings.add(matcher.group(12));
 				}
 			}
-			queryString = PATTERN_LINK.matcher(srcContent).replaceAll("11111");
+			queryString = PATTERN_LINK.matcher(srcContent).replaceAll("444");
 			queryString = URLEncoder.encode(queryString,"UTF-8");
 			final String getURL = GOOGLETRANSLATEURL_STRING+"?"+"msg="+queryString+"&tl="+TranslateActivity.targetLanguageString;
 			final HttpClient httpclient = new DefaultHttpClient();
 			final HttpGet httpGet = new HttpGet();
 			httpGet.setURI(new URI(getURL));
-//			final HttpPost httpPost = new HttpPost(GOOGLETRANSLATEURL_STRING);
-//			final ArrayList<NameValuePair> parameters = new ArrayList<NameValuePair>();
-//			parameters.add(new BasicNameValuePair("msg", queryString));
 			final HttpResponse response = httpclient.execute(httpGet);
 			return parseTranslateResponse(EntityUtils.toString(response.getEntity()));
 		} catch ( Exception e) {
